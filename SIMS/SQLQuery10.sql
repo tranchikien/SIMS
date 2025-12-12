1d24a9c5b16e61bd4f91d7dd6768757df1676269
tranchikien <araibarakirito@gmail.com>
2025-12-12 11:08:16 +0700

    Add files via upload

---
+++ SIMS/SQLQuery10.sql
@@ -0,0 +1,15 @@
+ALTER TABLE Students
+ADD Phone NVARCHAR(20),
+    Address NVARCHAR(200),
+    Gender NVARCHAR(10);
+
+ALTER TABLE Faculties
+ADD Phone NVARCHAR(20),
+    Address NVARCHAR(200),
+    Gender NVARCHAR(10);
+
+ALTER TABLE AdminProfiles
+ADD Phone NVARCHAR(20),
+    Address NVARCHAR(200),
+    Gender NVARCHAR(10);
+
